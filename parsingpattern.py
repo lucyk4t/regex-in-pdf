672bd8c7fbeb53b405582254a32eb208a0c515e4
lucyk4t <39478135+lucyk4t@users.noreply.github.com>
2019-06-25 09:57:16 +0900

    Update parsingpattern.py
    Constant values in tuples

--- parsingpattern.py
+++ parsingpattern.py
@@ -5,18 +5,19 @@ import sys
 import re
 
 GM_folder = "./GM"
-BMW_GS_folder = "./GS"
 
-CFR_PATTERN = re.compile(r'(\d)* CFR (\w)*')
-ECE_PATTERN = re.compile(r'ECE ((\w)*|(\d)*)')
-EEC_PATTERN = re.compile(r'(\d)*/(\d)*/EEC')
-EC_PATTERN = re.compile(r'(\d)*/(\d)*/EC')
-NCAP_PATTERN = re.compile(r'(\w)* NCAP')
-FMVSS_PATTERN = re.compile(r'FMVSS (\w)*')
-CMVSS_PATTERN = re.compile(r'CMVSS (\w)*')
-KMVSS_PATTERN = re.compile(r'KMVSS (\w)*')
-CONTRAN_PATTERN = re.compile(r'CONTRAN (\w)*')
-GB_PATTERN = re.compile(r'(GB/(\w)*)|(GB(\d)*)')
+global_spec_pattern_tuple = [
+    (r'(\d)* CFR (\w)*', 'CFR'),
+    (r'ECE ((\w)*|(\d)*)', 'ECE'),
+    (r'(\d)*/(\d)*/EEC', 'EEC'),
+    (r'(\d)*/(\d)*/EC', 'EC'),
+    (r'(\w)* NCAP', 'NCAP'),
+    (r'FMVSS (\w)*', 'FMVSS'),
+    (r'CMVSS (\w)*', 'CMVSS'),
+    (r'KMVSS (\w)*', 'KMVSS'),
+    (r'CONTRAN (\w)*', 'CONTRAN'),
+    (r'(GB/(\w)*)|(GB(\d)*)', 'GB')
+]
 
 if not sys.warnoptions:
     import warnings
@@ -32,26 +33,13 @@ def mkcpfile(pattern, filepath):
 
 
 def findpattern_pdf(content, filepath):
-    if CFR_PATTERN.findall(content):
-        mkcpfile('CFR', filepath)
-    if ECE_PATTERN.findall(content):
-        mkcpfile('ECE', filepath)
-    if EEC_PATTERN.findall(content):
-        mkcpfile('EEC', filepath)
-    if EC_PATTERN.findall(content):
-        mkcpfile('EC', filepath)
-    if NCAP_PATTERN.findall(content):
-        mkcpfile('NCAP', filepath)
-    if CONTRAN_PATTERN.findall(content):
-        mkcpfile('CORTRAN', filepath)
-    if FMVSS_PATTERN.findall(content):
-        mkcpfile('FMVSS', filepath)
-    if CMVSS_PATTERN.findall(content):
-        mkcpfile('CMVSS', filepath)
-    if KMVSS_PATTERN.findall(content):
-        mkcpfile('KMVSS', filepath)
-    if GB_PATTERN.findall(content):
-        mkcpfile('GB', filepath)
+    for pattern, value in global_spec_pattern_tuple:
+        pattern_compile = re.compile(pattern)
+
+        if re.search(pattern_compile, content):
+            mkcpfile(value, filepath)
+        else:
+            continue
     else:
         pass
 
@@ -66,8 +54,6 @@ def gatheringtext():
         fp = open(filepath, 'rb')
 
         if file.endswith('.pdf'):
-            # filepath = os.path.join(GM_folder, file)
-            # fp = open(filepath, 'rb')
             readpdf = PyPDF2.PdfFileReader(filepath)
             numpages = readpdf.getNumPages()
             page = readpdf.getPage(0)
