ba630e8ef379fc4fdc8de2c3772601bcb3d187ba
lucyk4t <39478135+lucyk4t@users.noreply.github.com>
2019-07-31 11:12:01 +0900

    Update parsingpattern.py

--- parsingpattern.py
+++ parsingpattern.py
@@ -1,71 +1,54 @@
-import PyPDF2
+import fitz
 import os
 import shutil
-import sys
+import os.path
+import glob
 import re
 
-GM_folder = "./GM"
+# using PyMUPDF library
+# for finding some pattern words in pdf files
+# funcion what is extended excel, ppt, txt in other office will be released soon
+# To-Be: PyQT5(GUI) + PyM
+def pymu_search_text():
+    filename = glob.iglob('./Spec/**/*.pdf', recursive=True)
 
-global_spec_pattern_tuple = [
-    (r'(\d)* CFR (\w)*', 'CFR'),
-    (r'ECE ((\w)*|(\d)*)', 'ECE'),
-    (r'(\d)*/(\d)*/EEC', 'EEC'),
-    (r'(\d)*/(\d)*/EC', 'EC'),
-    (r'(\w)* NCAP', 'NCAP'),
-    (r'FMVSS (\w)*', 'FMVSS'),
-    (r'CMVSS (\w)*', 'CMVSS'),
-    (r'KMVSS (\w)*', 'KMVSS'),
-    (r'CONTRAN (\w)*', 'CONTRAN'),
-    (r'(GB/(\w)*)|(GB(\d)*)', 'GB')
-]
+    for file in filename:
+        doc = fitz.open(file)
+        # page = doc.loadPage(0)
+        # page = doc[0]
+        # content = page.getText()
 
-if not sys.warnoptions:
-    import warnings
-    warnings.simplefilter("ignore")
+        # print('file: ', file, 'content: ', content)
+        global_spec_pattern_tuple = [
+            (r'(ECE.?R+.+)|(ECE \d+)', 'ECE'),
+            (r'(\d+ CFR.*)|(CFR \d*.+)', 'CFR'),
+            (r'CMVSS \d+', 'CMVSS'),
+            (r'FMVSS \w+', 'FMVSS'),
+            (r'KMVSS \w+', 'KMVSS'),
+            (r'(VDA \d+)|(VDA Volume .*)|(VDA Band .*)', 'VDA'),
+            (r'\d+/\d+/EC', 'EC'),
+            (r'\d+/\d+/EEC', 'EEC'),
+            (r'(GB/T \w+)|(GB\d+)', 'GB')
+        ]
 
-
-def mkcpfile(pattern, filepath):
-    try:
-        os.makedirs(r'./GM/' + pattern, exist_ok=True)
-        shutil.copy(filepath, r'./GM/' + pattern)
-    except FileExistsError:
-        pass
-
-
-def findpattern_pdf(content, filepath):
-    for pattern, value in global_spec_pattern_tuple:
-        pattern_compile = re.compile(pattern)
-
-        if re.search(pattern_compile, content):
-            mkcpfile(value, filepath)
-        else:
-            continue
+        for current_page in range(len(doc)):
+            page = doc.loadPage(current_page)
+            content = page.getText()
+            for pattern, value in global_spec_pattern_tuple:
+                precomp = re.compile(pattern)
+                if re.search(precomp, content):
+                    try:
+                        newdir = './' + value
+                        os.mkdir(newdir)
+                    except FileExistsError as e:
+                        pass
+                    finally:
+                        shutil.copy(file, newdir)
+                    # if page.searchFor('VDA'):  # fitz module provides searching method but it can't apply pattern.
+                else:
+                    continue
     else:
-        pass
+        print('done!')
 
-
-def findpattern_docx(content, filepath):
-    pass
-
-
-def gatheringtext():
-    for file in os.listdir(GM_folder):
-        filepath = os.path.join(GM_folder, file)
-        fp = open(filepath, 'rb')
-
-        if file.endswith('.pdf'):
-            readpdf = PyPDF2.PdfFileReader(filepath)
-            numpages = readpdf.getNumPages()
-            page = readpdf.getPage(0)
-            content = page.extractText()
-            findpattern_pdf(content, filepath)
-        elif file.endswith('.doc'):
-            pass
-        else:
-            continue
-        fp.close()
-    else:
-        print(r'Done!')
-
-
-gatheringtext()
+if __name__ == '__main__':
+    pymu_search_text()
